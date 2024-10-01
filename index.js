741c096c4d8b1e1688fe7860b9ea1e319e7e197c
animesh9777 <animesh9777@gmail.com>
2024-10-01 19:01:46 +0530

    uncommenting console.log

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function checkweather(city)
 {
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
     let data = await response.json();
-    // console.log(data);
+    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+ "°c";
